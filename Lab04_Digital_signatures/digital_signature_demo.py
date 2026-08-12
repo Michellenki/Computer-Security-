59c6563886b523a930445871f43ce3098642733f
bojack exploitman <122991527+Michellenki@users.noreply.github.com>
2026-08-13 02:06:33 +0300

    Update digital_signature_demo.py

--- Lab04_Digital_signatures/digital_signature_demo.py
+++ Lab04_Digital_signatures/digital_signature_demo.py
@@ -1,1 +1,25 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
 
+# Generate a private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+# Get the public key
+public_key = private_key.public_key()
+
+print("Private key generated!")
+print("Public key generated!")message = b"Computer Security Laboratory"
+
+print("\nOriginal message:")
+print(message.decode())signature = private_key.sign(
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)print("\nDigital signature:")
+print(signature)
