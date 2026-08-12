2618b40ea52a3a13b6bc3e0ae630a0c5603166ca
bojack exploitman <122991527+Michellenki@users.noreply.github.com>
2026-08-13 00:54:39 +0300

    wrong decryption key trial

--- Lab02-AES-Encryption/aes_demo.py
+++ Lab02-AES-Encryption/aes_demo.py
@@ -26,3 +26,15 @@ print(encrypted_message.decode())
 
 print("\nDecrypted message:")
 print(decrypted_message.decode())
+
+#try wrong decryption key
+
+wrong_key = Fernet.generate_key()
+wrong_cipher = Fernet(wrong_key)
+
+try:
+    wrong_decrypted = wrong_cipher.decrypt(encrypted_message)
+    print(wrong_decrypted.decode())
+
+except Exception:
+    print("\nDecryption with the wrong key FAILED.")
