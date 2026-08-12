60f9a6502e8f85bcb4fb62f50f03c8c3c1f6810d
bojack exploitman <122991527+Michellenki@users.noreply.github.com>
2026-08-13 01:31:35 +0300

    Create rsa_demo.py

---
+++ Lab 3/rsa_demo.py
@@ -0,0 +1,13 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate the private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+# Get the public key from the private key
+public_key = private_key.public_key()
+
+print("Private key generated!")
+print("Public key generated!")
